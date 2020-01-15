788e5d7016c5120b2a189e43b195031ca77737d2
bergi <bergi@axolotlfarm.org>
2020-01-15 01:05:56 +0100

    initial commit

---
+++ mutex.js
@@ -0,0 +1,26 @@
+const defer = require('./defer')
+
+class Mutex {
+  constructor () {
+    this._lock = null
+  }
+
+  async lock () {
+    if (this._lock) {
+      await this._lock.promise
+    }
+
+    this._lock = defer()
+  }
+
+  unlock () {
+    this._lock.resolve()
+    this._lock = null
+  }
+}
+
+function mutex () {
+  return new Mutex()
+}
+
+module.exports = mutex
